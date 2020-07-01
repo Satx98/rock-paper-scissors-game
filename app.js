be4b0017223da4f4bb80b96809632ee9125d505e
Satadru Roy <satadru.roy98@gmail.com>
2020-07-01 20:04:55 +0530

    Some concepts on functions

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+// const start = function() {          //////////////// Function Expressions: Storing functions in variables
+//   console.log('Game is starting...');     ////////////////  Function Expressions vs Function Declarations
+// };
+
+// const person = {                  //////////////// Functions Vs Methods
+//   name: 'Max',
+//   greet: function greet() {
+//     console.log('Hello there!');
+//   }
+// };
+
+// person.greet();
+
+// console.dir(startGame);          ///////////////// Functions are objects!
+
+startGameBtn.addEventListener('click', function() {     ////////////////// Anonymous functions
+  console.log('Game is starting...');
+});
